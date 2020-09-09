87cc33fabebad6b9d1c3e10607889910fb07d7f6
Li ZHENG <zpc@deakin.edu.au>
2020-09-09 23:29:25 +1000

    update

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
-const User = require('./models/User')
 const passport = require('passport') ,LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
 const session = require('express-session')
 const flash = require('connect-flash');
+const passportLocalMongoose = require ('passport-local-mongoose')
+const validator = require("validator");
+const SALT_WORK_FACTOR = 5;
 var problem = 0;
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"))
@@ -19,7 +21,129 @@ app.use(session({
 app.use(flash())
 app.use(passport.initialize())
 app.use(passport.session())
-mongoose.connect("mongodb://localhost/iCrowdTaskDB4", { useNewUrlParser: true,useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost/Test1", { useNewUrlParser: true,useUnifiedTopology: true});
+const userSchema = new mongoose.Schema({
+    country:{ 
+        type:String, 
+      //  required: true,
+        validate(value){
+            if(validator.isEmpty(value)){
+                problem = 1
+                throw new Error('Please input mobile!')
+            }
+        }
+    },
+    firstname: {
+        type: String,
+     //   required:  true ,
+        validate(value){
+            if(validator.isEmpty(value)){
+                problem = 1
+                throw new Error('Please input first name!')
+            }
+        }
+    },
+    lastname:{ 
+        type:String, 
+      //  required: true,
+        validate(value){
+            if(validator.isEmpty(value)){
+                problem = 1
+                throw new Error('Please input last name!')
+            }
+        }
+    },
+    email:{ 
+        type:String, 
+     //   required: true,
+        trim: true,
+        lowercase:true,
+        validate(value){
+             if(validator.isEmpty(value)){
+                 problem = 1
+                 throw new Error('Please input email!')
+             }
+            if(!validator.isEmail(value)){
+                problem = 4
+                throw new Error('Email is not valid!')
+            }
+        }
+    },
+    password: {
+        type: String,
+     //   required: true,
+        validate(value){
+           if(validator.isEmpty(value)){
+               problem = 1
+               throw new Error('Please input password!');
+           }
+             if(!validator.isLength(value,{min:8, max: 20})){
+                problem = 3
+                throw new Error('password should be at least 8 characters!')
+            }
+        }
+    },
+    address:{
+        type: String,
+       // required: true,
+        validate(value){
+            if(validator.isEmpty(value)){
+                problem = 1
+                throw new Error('Please input address!')
+            }
+        }
+    },
+    city:{
+        type: String,
+     //   required: true,
+        validate(value){
+            if(validator.isEmpty(value)){
+                problem = 1
+                throw new Error('Please input city!')
+            }
+        }
+    },
+    state:{
+        type: String,
+    //    required: true,
+        validate(value){
+            if(validator.isEmpty(value)){
+                problem = 1
+                throw new Error('Please input state!')
+            }
+        }
+    },
+    zip:{
+        type: String
+    },
+    mobile:{
+        type: String,
+        validate(value){
+            if((!validator.isEmpty(value))&&(!validator.isMobilePhone(value,))){
+                problem = 5
+                throw new Error('Phone number is not valid!')
+            }
+        }
+    },
+})
+
+userSchema.pre('save', function(next) {
+    var user = this;
+    if (!user.isModified('password')) return next();
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+
+userSchema.plugin(passportLocalMongoose);
+
+const User  =  mongoose.model("User", userSchema)
 passport.use('local', new LocalStrategy({
     usernameField:"email",
     passReqToCallback:true},
@@ -77,8 +201,8 @@ app.post('/register' , (req,res)=>{
         var user=new User(
             {
                 country:req.body.country,
-                firstname:req.body.lastname,
-                lastname:req.body.email,
+                firstname:req.body.firstname,
+                lastname:req.body.lastname,
                 email:req.body.email,
                 password:req.body.password,
                 address:req.body.address, 
@@ -89,9 +213,9 @@ app.post('/register' , (req,res)=>{
             }
         )
         user.save(function (error) {
-        if (error){
-            res.send(error)
-        }else if(problem == 1){
+            
+         if(error){
+        if(problem == 1){
             res.send("all inputs except Zip/Postal code and phone number are given!");
         }else if(req.body.password != req.body.confirmpassword){
             res.send("Confirm password need to be the same as password!");      
@@ -99,6 +223,7 @@ app.post('/register' , (req,res)=>{
             res.send("The password must be at least 8 characters!");            
         }else if(problem == 4 || problem == 5){
             res.send("The email address and mobile phone number must be valid!");
+        }
         }else{
             res.send("Register successfully!");
         }
@@ -113,14 +238,11 @@ app.post('/reregister' , (req,res)=>{
 app.post('/forgetpassword' , (req,res)=>{
     res.redirect("forgetpassword")
 })
-app/post('/save' , (req,res)=>{
-
-})
 let port = process.env.PORT;
  if (port == null || port == "") {
    port = 8000;
  }
 
-app.listen(8000, (req,res)=>{
+app.listen(port, (req,res)=>{
     console.log("Server is running successfullly!")
 })
